a2928809e2e08549c6b00266a03a4210e6985bf7
rohandasari <rohandasari098@gmail.com>
2024-07-09 12:26:18 +0530

    assignment

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const admin = require('firebase-admin');
-const serviceAccount = require("./storeData.json");
+
 
 const app = express();
 const port = 7000
